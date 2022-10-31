29c66b36de22c0752916a4c941ca2cff39151b36
Christian Luna <christianlunaduenas@gmail.com>
2022-10-31 16:42:37 -0700

    The start of index.html and script.js/index.js

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var APIKey = "9d8127ce22abf5a5e62568487d3a430b";
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+var city;
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
